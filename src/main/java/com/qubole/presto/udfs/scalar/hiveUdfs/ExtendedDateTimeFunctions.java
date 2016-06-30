6731190aa5c2b34aefb667826028b5d4bce0af3d
Rajat Venkatesh <rvenkatesh@qubole.com>
2016-06-30 15:43:49 -0700

    Package can be created

--- src/main/java/com/qubole/presto/udfs/scalar/hiveUdfs/ExtendedDateTimeFunctions.java
+++ src/main/java/com/qubole/presto/udfs/scalar/hiveUdfs/ExtendedDateTimeFunctions.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.qubole.udfs.scalar.hiveUdfs;
+package com.qubole.presto.udfs.scalar.hiveUdfs;
 
 import com.facebook.presto.operator.Description;
 import com.facebook.presto.operator.scalar.ScalarFunction;
